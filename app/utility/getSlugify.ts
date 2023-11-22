81059fa1dd6562cb16d6f547e9db1d6ad5417cb7
Rajdeep Singh <radewala.2@gmail.com>
2023-11-22 15:10:00 +0530

    update the option

--- app/utility/getSlugify.ts
+++ app/utility/getSlugify.ts
@@ -2,8 +2,11 @@ import slugify from "slugify";
 
 export function getSlugify(title:string) {
     return slugify(title, {
+        replacement:"-",
         lower: true,
         trim: true,
+        strict: true,
+        locale: "en",
         remove: /[*+~.()'"!:@]/g
       })
 }
