9dcdd7064d0fe4a4f41f8a5fa49d5c25412e8fb0
Rajdeep Singh <radewala.2@gmail.com>
2023-11-25 18:02:16 +0530

    fix the date type and update the ui

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { URL } from "@/app/utility/getURL"
 import dayjs from "dayjs"
 
 async function getData() {
+
   const res = await fetch(`${URL}api/posts`);
 
   if (!res.ok) {
@@ -17,7 +18,7 @@ async function getData() {
 interface PostsType {
   id: number;
   title: string;
-  date: string;
+  date: Date;
   body: string;
   userId: number;
   tags: string[];
@@ -32,25 +33,27 @@ export default async function Home() {
 
   let convertIntoSlug = isTitle.toLocaleLowerCase().trim().replaceAll(" ", "-");
 
-  let getCurrentDate = dayjs().format("MMM DD, YYYY"); // https://day.js.org/docs/en/parse/string-format
+  let getCurrentDate = dayjs().format(); // https://day.js.org/docs/en/parse/string-format
+
 
   return (
     <main className={styles.main}>
-      <h1 className={styles.h1}> Manually </h1>
+      <h1 className={styles.h1}> Current date </h1>
 
       <div className={styles.card}>
-      <time className="text-sm" dateTime={getCurrentDate} title={getCurrentDate}>
-              {getCurrentDate}
-            </time>
+        <time className="text-sm my-3" dateTime={getCurrentDate} title={getCurrentDate}>
+          {getCurrentDate}
+        </time>
         <h2> How to slugify the title or sting in JavaScript? </h2>
         <p>{convertIntoSlug}</p>
       </div>
 
-      <h1 className={styles.h1}> Using NPM package </h1>
+      <h1 className={styles.h1}> Convert ISO8601 Date	into human-readable format. </h1>
 
       {posts.map((post: PostsType) => {
         let slug = "/post/" + getSlugify(post.title)
         let getDate = dayjs(post.date).format("MMM DD, YYYY");
+        console.log(post.date, " and ", getDate)
         return (
           <div key={post.id} className={styles.card}>
             <time className="text-sm" dateTime={getDate} title={getDate}>
